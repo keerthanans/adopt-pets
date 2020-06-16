1fcb784ef9d30fe06fbfb9ababe4569b31bfbbfb
keerthanans <k.sudalaimani@perficient.com>
2020-06-16 10:05:33 +0530

    first commit

---
+++ src/data/cats.js
@@ -0,0 +1,46 @@
+/* eslint-disable indent */
+export default [
+    {
+        name: 'Sheeba',
+        breed: 'collie',
+		species: 'cat',
+        gender: 'female',
+        age: 7,
+        color: 'black/white',
+        weight: 34,
+        location: 'fourside',
+        notes: 'Pure breed, trained for competitions'
+    },
+    {
+        name: 'Hillary',
+        breed: 'nut',
+		 species: 'cat',
+        gender: 'female',
+        age: 17,
+        color: 'Orange/white',
+        weight: 37,
+        location: 'threed',
+        notes: 'Super friendly'
+    },
+    {
+        name: 'Sheeba',
+        breed: 'collie',
+		 species: 'cat',
+        gender: 'female',
+        age: 7,
+        color: 'black/white',
+        weight: 34,
+        location: 'fourside',
+        notes: 'Pure breed, trained for competitions'
+    }, {
+        name: 'Hillary',
+        breed: 'nut',
+		 species: 'cat',
+        gender: 'female',
+        age: 17,
+        color: 'Orange/white',
+        weight: 37,
+        location: 'threed',
+        notes: 'Super friendly'
+    }
+]
